64427348262f70697847e3a16d7d5013d6f07f0f
Alberto Elorza <albertoelorza2002@gmail.com>
2023-05-21 20:47:42 +0200

    mapping films

--- lib/film_affinity_scraper.ex
+++ lib/film_affinity_scraper.ex
@@ -1,4 +1,5 @@
 defmodule FilmAffinityScraper do
+  alias FilmAffinityScraper.Actions
   alias FilmAffinityScraper.Pages.{Film, MyList, UserList}
 
   defdelegate get_film(id, opts \\ []), to: Film
@@ -6,4 +7,10 @@ defmodule FilmAffinityScraper do
   defdelegate get_my_list(id, opts \\ []), to: MyList, as: :get_list
 
   defdelegate get_user_list(user_id, list_id, opts \\ []), to: UserList, as: :get_list
+
+  defdelegate collect_films(list), to: Actions
+
+  defdelegate map_films(list, action), to: Actions
+
+  defdelegate get_list(opts \\ []), to: Actions
 end
--- lib/film_affinity_scraper/actions.ex
+++ lib/film_affinity_scraper/actions.ex
@@ -1,2 +1,16 @@
 defmodule FilmAffinityScraper.Actions do
+  alias FilmAffinityScraper.Pages.{Film, MyList, UserList}
+
+  def collect_films(list), do: Enum.map(list, &Film.get_film/1)
+
+  def map_films(list, action), do: Enum.map(list, &(action.(Film.get_film(&1))))
+
+  @spec get_list([id: term(), user: term()]) :: list(term())
+  def get_list(opts \\ []) do
+    case {opts[:id], opts[:user]} do
+      {nil, _} -> {:err, "expecting list id"}
+      {id, nil} -> MyList.get_list(id, cookie: opts[:cookie])
+      {id, user} -> UserList.get_list(user, id)
+    end
+  end
 end
